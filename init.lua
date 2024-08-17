cb1627cf777d38942620c30810bf69dedc9e5162
tiza <main@tiza.io>
2024-08-17 12:45:45 -0600

    Put all lazy plugin manager related stuff in the lazy directory

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require 'lazy-bootstrap'
-require 'lazy-plugins'
+require 'lazy/lazy-bootstrap'
+require 'lazy/lazy-plugins'
 require 'opts/opts'
 require 'keys'
